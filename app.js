ff81a2f2a92ae42b227c207e55f402c9e7dd77af
joepars7 <parsleyjoseph7@gmail.com>
2023-12-20 02:15:49 -0300

    [FEAT] method override and body parsers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const methodOverride = require('method-override');
 
 const mainRoutes = require('./src/routes/mainRoutes')
@@ -9,10 +10,16 @@ const shopRoutes = require('./src/routes/authRoutes')
 
 const PORT = 3004;
 
-app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirmame), './src/views');
+
+
 app.use(express.urlencoded());
 app.use(express.json())
 app.use(methodOverride('_method'));
+
+app.use(express.static('public'));
+
 
 app.use('/', mainRoutes);
 app.use('/shop', shopRoutes);
@@ -20,4 +27,4 @@ app.use('/admin', adminRoutes);
 app.use('/admin', authRoutes);
 
 app.get('/ping', (req, res) => res.send('pong'));
-app.listen(4000, () => console.log("Servidor corriendo en http://localhost:4000"));
+app.listen(PORT, () => console.log("Servidor corriendo en http://localhost:$(PORT)"));
